0c485c9d84ed97b0b9c327ec3aebc88ec66b41fa
Pascal Rengelshausen <pascal.rengelshausen@gmail.com>
2019-04-08 17:59:13 +0200

    LogIn Klasse neu erstellt

---
+++ Zehnfingersystem/LogIn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zehnfingersystem
+{
+    class LogIn : MainWindow
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Passwort { get; set; }
+
+        public void Daten()
+        {
+            Username = username.Text;
+            Email = email.Text;
+            Passwort = passwort.Text;
+        }
+    }
+}
